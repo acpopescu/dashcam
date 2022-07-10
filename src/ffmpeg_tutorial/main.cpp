902c72612224f50b0aa3dc55cc69c5aa54c9a9cc
Adrian Popescu <adrian-constantin.popescu@outlook.com>
2022-07-10 08:21:16 -0400

    First Tutorial Pass - Open video and print codec

--- src/ffmpeg_tutorial/main.cpp
+++ src/ffmpeg_tutorial/main.cpp
@@ -3,11 +3,14 @@
 #include <future>
 #include <string>
 #include <fmt/format.h>
+#include <vector>
+#include "utils/CommandLine.h"
+
+extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
-#include <vector>
-
+}
 
 
 
@@ -31,17 +34,69 @@ void modeltest(int n)
         }
     }
 }
+
+void printFFMPEGError(int result, const std::string & inFileName)
+{
+    char avErrorBuffer[AV_ERROR_MAX_STRING_SIZE+1] = {0};
+    av_strerror(result,avErrorBuffer,AV_ERROR_MAX_STRING_SIZE);    
+    fmt::print("Error processing {}:{}\n", inFileName, avErrorBuffer);
+}
+
 int main(int argc, char ** argv)
 {
-    if( argc<=2 )
+    const CommandLine cmd(argc, argv);
+    if( argc<=2 || cmd["in"]=="")
     {
-        fmt::print("Usage:{} <out file>", argv[0]);
+        fmt::print("Usage:{} --in=<in file>", argv[0]);
         return -1;
     }
-    std::string outputFileName = argv[1];
-    char *x= new char[1000];
+    std::string inFileName = cmd["in"];
+    
+    AVFormatContext * inAVFormatContext = avformat_alloc_context();
+    int result = avformat_open_input(&inAVFormatContext, inFileName.c_str(), nullptr, nullptr);
+    if(result<0)
+    {
+        printFFMPEGError(result, inFileName);
+        return result;
+    }
+    
+    result = avformat_find_stream_info(inAVFormatContext, nullptr);
+    if(result<0)
+    {
+        printFFMPEGError(result, inFileName);
+        return result;
+    }
 
+    fmt::print("File format : {}. Stream count: {}. Duration microseconds: {}\n", 
+        inAVFormatContext->iformat->long_name, 
+        inAVFormatContext->nb_streams,
+        inAVFormatContext->duration);
 
-    delete []x;
+    for(uint32_t streamIdx=0; streamIdx < inAVFormatContext->nb_streams; streamIdx++)
+    {
+        AVStream * stream =  inAVFormatContext->streams[streamIdx];
+        AVCodecParameters * codecParams = stream->codecpar;
+        AVCodec * codec = avcodec_find_decoder(codecParams->codec_id);
+        if(codec)
+        {
+            switch(codec->type)
+            {
+                case AVMEDIA_TYPE_AUDIO:
+                    fmt::print("{:>2} {:>8} {:>8}bps {:>4}x{:<6}\n", streamIdx, codec->name, codecParams->bit_rate, codecParams->channels, codecParams->sample_rate);
+                    break;
+                case AVMEDIA_TYPE_VIDEO:
+                    fmt::print("{:>2} {:>8} {:>8}bps {:>4}x{:<6}\n", streamIdx, codec->name, codecParams->bit_rate, codecParams->width, codecParams->height);
+                    break;
+                default:
+                    fmt::print("{:>2} {:>8} {:>8}bps {:>4}x{:<6}\n", streamIdx, codec->name, codecParams->bit_rate, 0, 0);
+                    break;
+            }
+            
+        }
+        else
+        {
+            fmt::print("{:>2} {:>8} {>:8}bps {:>4}x{:<6}\n", streamIdx, "null", 0, 0, 0);
+        }
+    }
     return 0; 
 }
